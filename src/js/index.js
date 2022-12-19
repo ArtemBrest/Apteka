ec519e7130f414d76fb662585cfc03554828c152
Artem Zan <artoym.zan@gmail.com>
2022-12-19 23:34:07 +0300

    add account info page

--- src/js/index.js
+++ src/js/index.js
@@ -71,6 +71,10 @@ function getCookie(name) {
     ));
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
+const regexEmail = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+const regexPhone = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){9,12}(\s*)?$/;
+const regexSubject = /^[а-яёА-Яa-zA-Z0-9 .,!?:'"+_&@#*()-]{2,1000}$/iu;
+
 window.addEventListener("load", function () {
     let fade = document.getElementById("fade");
     let header = document.getElementById("header");
@@ -946,7 +950,7 @@ window.addEventListener("load", function () {
         })
     }
 
-    const regexEmail = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+
     let inputsEmail = document.querySelectorAll(".input-email");
     inputsEmail.forEach(function (input){
         input.addEventListener('keyup', () => {
@@ -1101,11 +1105,6 @@ window.addEventListener("load", function () {
         }
     }
 
-
-
-    const regexSubject = /^[а-яёА-Яa-zA-Z0-9 .,!?:'"+_&@#*()-]{2,1000}$/iu;
-    const regexPhone = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){9,12}(\s*)?$/;
-
     const feedbackForm = document.getElementById('feedback-form');
     const feedbackFormName = document.getElementById('feedback-name');
     const feedbackFormEmail = document.getElementById('feedback-email');
@@ -1190,7 +1189,7 @@ window.addEventListener("load", function () {
             if(feedbackFormCompany !== null){
                 feedbackFormCompanyValue = feedbackFormCompany.value
             }
-            if (!regexSubject.test(feedbackFormName.value) || !regexEmail.test(feedbackFormEmail.value) || !regexPhone.test(feedbackFormPhone.value) || !regexSubject.test(feedbackFormCompanyValue)) { //|| radioValue == "Выберете тему обращения"
+            if (!regexSubject.test(feedbackFormName.value) || !mail.test(feedbackFormEmail.value) || !regexPhone.test(feedbackFormPhone.value) || !regexSubject.test(feedbackFormCompanyValue)) { //|| radioValue == "Выберете тему обращения"
                 if (!regexSubject.test(feedbackFormName.value)) {
                     feedbackFormName.parentNode.classList.add("error");
                 }
@@ -1561,6 +1560,242 @@ window.addEventListener("load", function () {
                 el.classList.toggle("active");
                 parent.classList.toggle("active");
             })
+        })
+    }
+
+    let accountInfoInput = document.querySelectorAll(".account-information__input--readonly");
+    if(!isEmptyObject(accountInfoInput)){
+        accountInfoInput.forEach(function (input) {
+            let parent = input.parentNode;
+            let icon = parent.querySelector(".account-information__icon");
+            icon.addEventListener("click", function (e){
+                parent.classList.add("change");
+                input.removeAttribute("readonly");
+                input.focus();
+            })
+        })
+        let accountNameInput = document.getElementById("name-input");
+        accountNameInput.addEventListener('keyup', () => {
+            let parent = accountNameInput.parentNode;
+            if (regexSubject.test(accountNameInput.value)) {
+                parent.classList.remove("error");
+                parent.classList.add("valid");
+            } else {
+                parent.classList.remove("valid");
+                parent.classList.add("error");
+            }
+        });
+        let accountEmailInput = document.getElementById("email-input");
+        accountEmailInput.addEventListener('keyup', () => {
+            let parent = accountEmailInput.parentNode;
+            if (regexEmail.test(accountEmailInput.value)) {
+                parent.classList.remove("error");
+                parent.classList.add("valid");
+            } else {
+                parent.classList.remove("valid");
+                parent.classList.add("error");
+            }
+        });
+        let accountPhoneInput = document.getElementById("phone-input");
+        accountPhoneInput.addEventListener('keyup', () => {
+            let parent = accountPhoneInput.parentNode;
+            if (regexPhone.test(accountPhoneInput.value)) {
+                parent.classList.remove("error");
+                parent.classList.add("valid");
+            } else {
+                parent.classList.remove("valid");
+                parent.classList.add("error");
+            }
+        });
+        /*document.querySelector(".account-information__btn").addEventListener("click",function (e) {
+
+        })*/
+    }
+    let accountPasswordInput = document.querySelectorAll(".account-password__input");
+    if(!isEmptyObject(accountPasswordInput)){
+        accountPasswordInput.forEach(function (input) {
+            let parent = input.parentNode;
+            let passwordBtnClose = parent.querySelector(".password-btn-close");
+            let passwordBtnOpen = parent.querySelector(".password-btn-open");
+            passwordBtnClose.addEventListener("click", function (e){
+                passwordBtnClose.classList.add("hidden");
+                passwordBtnOpen.classList.remove("hidden");
+                parent.querySelector("input").type = "text";
+            })
+            passwordBtnOpen.addEventListener("click", function (e){
+                passwordBtnOpen.classList.add("hidden");
+                passwordBtnClose.classList.remove("hidden");
+                parent.querySelector("input").type = "password";
+            })
+            input.addEventListener("keyup", () => {
+                if (input.value.length >= 8) {
+                    parent.classList.remove("error");
+                    parent.classList.add("valid");
+                } else {
+                    parent.classList.remove("valid");
+                    parent.classList.add("error");
+                }
+
+            })
+            document.querySelector(".account-password__btn").addEventListener("click",function (e) {
+                if(document.getElementById("new-password").value.length < 1){
+                    return;
+                }
+                else if(document.getElementById("new-password").value == document.getElementById("confirm-password").value){
+                    document.getElementById("confirm-password").parentNode.classList.remove("error");
+                    document.getElementById("confirm-password").parentNode.classList.add("valid");
+                }
+                else{
+                    document.getElementById("confirm-password").parentNode.classList.remove("valid");
+                    document.getElementById("confirm-password").parentNode.classList.add("error");
+                }
+            })
+        })
+    }
+
+
+    /// calendar
+    let Cal = function(divId) {
+        //Сохраняем идентификатор div
+        this.divId = divId;
+        // Дни недели с понедельника
+        this.DaysOfWeek = [
+            'Пн',
+            'Вт',
+            'Ср',
+            'Чт',
+            'Пт',
+            'Сб',
+            'Вс'
+        ];
+        // Месяцы начиная с января
+        this.Months =['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+        //Устанавливаем текущий месяц, год
+        let d = new Date();
+        this.currMonth = d.getMonth('9');
+        this.currYear = d.getFullYear('22');
+        this.currDay = d.getDate('3');
+    };
+    // Переход к следующему месяцу
+    Cal.prototype.nextMonth = function() {
+        if ( this.currMonth == 11 ) {
+            this.currMonth = 0;
+            this.currYear = this.currYear + 1;
+        }
+        else {
+            this.currMonth = this.currMonth + 1;
+        }
+        this.showcurr();
+    };
+    // Переход к предыдущему месяцу
+    Cal.prototype.previousMonth = function() {
+        if ( this.currMonth == 0 ) {
+            this.currMonth = 11;
+            this.currYear = this.currYear - 1;
+        }
+        else {
+            this.currMonth = this.currMonth - 1;
+        }
+        this.showcurr();
+    };
+    // Показать текущий месяц
+    Cal.prototype.showcurr = function() {
+        this.showMonth(this.currYear, this.currMonth);
+    };
+    // Показать месяц (год, месяц)
+    Cal.prototype.showMonth = function(y, m) {
+        let d = new Date()
+            // Первый день недели в выбранном месяце
+            , firstDayOfMonth = new Date(y, m, 7).getDay()
+            // Последний день выбранного месяца
+            , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
+            // Последний день предыдущего месяца
+            , lastDayOfLastMonth = m == 0 ? new Date(y-1, 11, 0).getDate() : new Date(y, m, 0).getDate();
+        let html = '<table>';
+        // Запись выбранного месяца и года
+        html += '<thead><tr>';
+        html += '<td colspan="7">' + this.Months[m] + ' ' + y + '</td>';
+        html += '</tr></thead>';
+        // заголовок дней недели
+        html += '<tr class="days">';
+        for(let i = 0; i < this.DaysOfWeek.length; i++) {
+            html += '<td>' + this.DaysOfWeek[i] + '</td>';
+        }
+        html += '</tr>';
+        // Записываем дни
+        let i =1;
+        do {
+            let dow = new Date(y, m, i).getDay();
+            // Начать новую строку в понедельник
+            if ( dow == 1 ) {
+                html += '<tr>';
+            }
+            // Если первый день недели не понедельник показать последние дни предидущего месяца
+            else if ( i == 1 ) {
+                html += '<tr>';
+                let k = lastDayOfLastMonth - firstDayOfMonth+1;
+                for(let j = 0; j < firstDayOfMonth; j++) {
+                    html += '<td class="not-current">' + k + '</td>';
+                    k++;
+                }
+            }
+            // Записываем текущий день в цикл
+            let chk = new Date();
+            let chkY = chk.getFullYear();
+            let chkM = chk.getMonth();
+            if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
+                html += '<td data-date="' + i + '.'+ Number(1 +  m) +'.'+ y +'" class="today calendar-day">' + i + '</td>';
+            } else {
+                html += '<td data-date="' + i + '.'+ Number(1 + m) +'.'+ y +'" class="normal calendar-day">' + i + '</td>';
+            }
+            // закрыть строку в воскресенье
+            if ( dow == 0 ) {
+                html += '</tr>';
+            }
+            // Если последний день месяца не воскресенье, показать первые дни следующего месяца
+            else if ( i == lastDateOfMonth ) {
+                let k = 1;
+                for(dow; dow < 7; dow++) {
+                    html += '<td class="not-current">' + k + '</td>';
+                    k++;
+                }
+            }
+            i++;
+        }
+        while(i <= lastDateOfMonth);
+        // Конец таблицы
+        html += '</table>';
+        // Записываем HTML в div
+        document.getElementById(this.divId).innerHTML = html;
+    };
+    // Начать календарь
+    let c = new Cal("divCal");
+    c.showcurr();
+    // Привязываем кнопки «Следующий» и «Предыдущий»
+    document.getElementById('btnNext').onclick = function() {
+        c.nextMonth();
+    };
+    document.getElementById('btnPrev').onclick = function() {
+        c.previousMonth();
+    };
+    let calendarTable = document.getElementById("account-calendar");
+    let dateInput = document.getElementById("date-input");
+    let accountInfoItemDate = document.querySelector(".account-information__item--date")
+    if(calendarTable !== null && dateInput !== null){
+        document.body.addEventListener("click", function (e) {
+            let target = e.target.closest('.calendar-day');
+            if(target !== null){
+                let curDate = target.getAttribute("data-date");
+                dateInput.value = curDate;
+            }
+        })
+        accountInfoItemDate.addEventListener("click", function (e) {
+            if(e.target.closest("#btnPrev, #btnNext")){
+                return;
+            }
+            else{
+                calendarTable.classList.toggle("account-calendar--open")
+            }
         })
     }
 })
